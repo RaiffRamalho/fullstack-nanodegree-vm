851868fae9aaed1bb8da1b7d556edda890a8084f
raiff <rmello@lenovo.com>
2018-07-13 14:35:28 -0300

    Crud Genre ok

--- vagrant/project/app.py
+++ vagrant/project/app.py
@@ -10,13 +10,14 @@ engine = create_engine('sqlite:///musicgenrewithusers.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
 
-#home route
+
+#home route show all bands
 @app.route('/')
 @app.route('/genre')
 def showGenre():
+    session = DBSession()
     genres = session.query(Genre).all()
     return render_template('music/genres.html', genres=genres)
 
@@ -24,23 +25,59 @@ def showGenre():
 #route to get bands of a genre
 @app.route('/genre/<int:genre_id>/')
 def genreBands(genre_id):
+    session = DBSession()
     genre = session.query(Genre).filter_by(id=genre_id).one()
     bands = session.query(Band).filter_by(genre_id=genre.id)
     return render_template('genre/bands.html',genre=genre, bands=bands)
 
-# @app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
-# def newMenuItem(restaurant_id):
+@app.route('/genre/new/', methods=['GET', 'POST'])
+def newGenre():
+    session = DBSession()
+    if request.method == 'POST':
+        newGenre = Genre(name=request.form['name'], user_id=1)
+        session.add(newGenre)
+        session.commit()
+        return redirect(url_for('showGenre'))
+    else:
+        return render_template('music/newGenre.html')
+
+
+@app.route('/genre/<int:genre_id>/edit/', methods=['GET', 'POST'])
+def editGenre(genre_id ):
+    session = DBSession()
+    editedGenre = session.query(Genre).filter_by(id = genre_id).one()
+
+    if request.method == 'POST':
+        if request.form['name']:
+            editedGenre.name = request.form['name']
+        session.add(editedGenre)
+        session.commit() 
+        return redirect(url_for('showGenre'))
+    else:
+        return render_template('music/editGenre.html', genre = editedGenre)
+
+
+@app.route('/genre/<int:genre_id>/delete/', methods=['GET', 'POST'])
+def deleteGenre(genre_id):
+    session = DBSession()
+    genreToDelete = session.query(Genre).filter_by(id=genre_id).one()
+    if request.method == 'POST':
+        session.delete(genreToDelete)
+        session.commit()
+        return redirect(url_for('showGenre'))
+    else:
+        return render_template('music/deleteconfirmation.html', genre=genreToDelete)
+
+# @app.route('/genre/<int:genre_id>/new/', methods=['GET', 'POST'])
+# def newMenuItem(genre_id):
 #     if request.method == 'POST':
-#         newItem = MenuItem(
-#             name=request.form['name'], restaurant_id=restaurant_id)
-#         session.add(newItem)
+#         genre = session.query(Genre).filter_by(id=genre_id).one()
+#         newBand = Band(name=request.form['name'], description=request.form['description'], genre_id=genre_id)
+#         session.add(newBand)
 #         session.commit()
-#         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+#         return redirect(url_for('genre/bands.html', genre=genre))
 #     else:
-#         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
-
-# , methods = ['GET', 'POST']
-
+#         return render_template('newmenuitem.html', genre=genre)
 
 
 
